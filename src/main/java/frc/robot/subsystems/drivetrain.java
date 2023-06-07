0a486e645142e5dc0fcf14caad507ca2e86521ad
Ziv Rada <zivrada2221@gmail.com>
2023-06-07 18:49:26 +0300

    0_0 not much just removed a useless import

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
